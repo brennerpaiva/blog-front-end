f966d492c5c22e15316fea59e067b61dc93f5bfc
unknown <brennerpaiva@gmail.com>
2023-06-15 18:17:46 -0300

    create navbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo-blog.png';
+import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a>
+          <Image src={logo} alt="Sujeito Programador Logo" />
+        </a>
+
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <span>Home</span>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <span>Conteúdos</span>
+          </ActiveLink>
+          <ActiveLink href="/about" activeClassName={styles.active}>
+            <span>Quem somos?</span>
+          </ActiveLink>
+        </nav>
+
+        <a className={styles.readyButton} type="button" href="#">
+          COMEÇAR
+        </a>
+      </div>
+    </header>
+  );
+}
