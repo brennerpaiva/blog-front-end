e2eb4dffeb81082ffb8b5dc4d89a299ec5928caf
unknown <brennerpaiva@gmail.com>
2023-06-16 11:12:34 -0300

    create section home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Front-end Blog</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por Tencologia - Brenner Paiva</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao proximo nível!</h1>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam,
+              dignissimos quam nesciunt sed tenetur quia facere ratione labore
+              provident quod sunt rem consequuntur exercitationem.
+            </span>
+            <a>
+              <button>COMEÇAR AGORA!</button>
+            </a>
+          </section>
+          <img src="/images/banner-conteudos.png" alt="Conteúdos" />
+        </div>
+      </main>
+    </>
   );
 }
