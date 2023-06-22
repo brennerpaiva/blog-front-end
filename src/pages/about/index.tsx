f8216d419691b254bc9509e77ef3529ec4c5eae8
unknown <brennerpaiva@gmail.com>
2023-06-22 14:26:28 -0300

    add icon and link to github

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss';
 import Head from 'next/head';
 import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 export default function About() {
   return (
@@ -13,16 +14,21 @@ export default function About() {
           <section className={styles.ctaText}>
             <h1>Front-End Insights</h1>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
-              natus molestiae doloribus voluptate perspiciatis placeat quia
-              facere! Sapiente officia rerum molestias eius libero, ipsum
-              asperiores vero dolorum, mollitia, sunt assumenda.
+              O Front-end Insights acredita na importância da aprendizagem
+              contínua e no compartilhamento de conhecimento. Queremos Capacitar
+              desenvolvedores front-end, iniciantes e experientes, a alcançarem
+              seus objetivos e se destacarem em suas carreiras. Compartilhamos
+              nossas experiências, insights e truques do ofício para ajudá-lo a
+              enfrentar os desafios do desenvolvimento front-end.
             </p>
             <a
               href="https://www.linkedin.com/in/brenner-paiva1/"
               target="_blank"
             >
               <FaLinkedin />
+            </a>
+            <a href="https://github.com/brennerpaiva" target="_blank">
+              <FaGithub />
             </a>
           </section>
           <img src="images/about-us.png" alt="about us" />
